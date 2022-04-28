498e0dc2db84b5c82eaed821522cb576ebc270bc
Andrew <andrew.narolsky@gmail.com>
2022-04-28 12:53:58 +0300

    add country-filter to api-links

--- app/Http/Controllers/API/FootballClubController.php
+++ app/Http/Controllers/API/FootballClubController.php
@@ -2,19 +2,23 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Models\Country;
 use App\Models\FootballClub;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class FootballClubController extends ApiController
 {
+    protected object $country;
     protected object $footballClub;
     protected object $request;
 
     public function __construct(
+        Country $country,
         FootballClub $footballClub,
         Request $request)
     {
+        $this->country = $country;
         $this->footballClub = $footballClub;
         $this->request = $request;
     }
@@ -27,7 +31,12 @@ class FootballClubController extends ApiController
 
     public function getModels() : Response
     {
-        $footballClubs = $this->footballClub::all();
+        if ($this->request->input('country')) {
+            $footballClubs = $this->country::with('footballClubs')
+                ->findorfail($this->request->input('country'))->footballClubs;
+        } else {
+            $footballClubs = $this->footballClub::all();
+        }
         return response($footballClubs, Response::HTTP_OK);
     }
 }
